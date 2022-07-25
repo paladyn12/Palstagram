4de3879352e1a02970e16b9630a23734de8c8bad
Taewan Kim <paladyn12@naver.com>
2022-07-25 17:31:20 +0900

    크래시 수정

--- app/src/main/java/com/example/palstagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/palstagram/navigation/DetailViewFragment.kt
@@ -37,6 +37,8 @@ class DetailViewFragment : Fragment(){
             firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                 contentDTOs.clear()
                 contentUidList.clear()
+                //Sometimes, this code return null of querySnapshot when it signout
+                if (querySnapshot == null) return@addSnapshotListener
                 for(snapshot in querySnapshot!!.documents){
                     var item = snapshot.toObject(ContentDTO::class.java)
                     contentDTOs.add(item!!)
